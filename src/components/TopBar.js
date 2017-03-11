9984b633ae7bbc7e93a6fad29a29520fbec8c68f
Patrick Galbraith <patrick.j.galbraith@gmail.com>
2017-03-11 18:06:19 +1030

    Add initial TopBar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,7 +1,46 @@
 import React, { Component, PropTypes } from 'react'
+import { toggleFullScreen } from '../helpers'
+import { POSTER_LINK } from '../constants'
 
 export default class TopBar extends Component {
+  openUrl(url) {
+    window.open(url)
+  }
+
   render() {
-    return <div className='TopBar' />
+    const menuItems = [
+      {
+        title: 'legend',
+        onClick: () => {}
+      },
+      {
+        title: 'share/follow',
+        onClick: () => {}
+      },
+      {
+        title: 'fullscreen',
+        onClick: toggleFullScreen
+      },
+      {
+        title: 'settings',
+        onClick: () => {}
+      },
+      {
+        title: 'buy poster',
+        onClick: () => { this.openUrl(POSTER_LINK) }
+      },
+      {
+        title: 'about',
+        onClick: () => {}
+      }
+    ]
+
+    return (
+      <ul className='TopBar'>
+        { menuItems.map((item, idx) => (
+          <li key={idx} className='TopBar-item'><button onClick={item.onClick}>{ item.title }</button></li>
+        ))}
+      </ul>
+    )
   }
 }
---
+++ src/helpers.js
@@ -0,0 +1,24 @@
+// https://gist.github.com/simonewebdesign/6183356
+export function toggleFullScreen() {
+  if (
+    !document.fullscreenElement &&
+    !document.mozFullScreenElement &&
+    !document.webkitFullscreenElement
+  ) {
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen()
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen()
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT)
+    }
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen()
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen()
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen()
+    }
+  }
+}
