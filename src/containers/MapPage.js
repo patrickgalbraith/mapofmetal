d75cbc909e4f6d90aed52181a4838e09e7b45725
Patrick Galbraith <patrick.j.galbraith@gmail.com>
2017-03-25 18:18:21 +1030

    Fix ReactCSSTransitionGroup error

--- src/containers/MapPage.js
+++ src/containers/MapPage.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 import VideoPlayer from './VideoPlayer'
 import TopBar from '../components/TopBar'
